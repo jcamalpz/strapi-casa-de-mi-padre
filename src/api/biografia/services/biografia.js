c32505b4a8cb92a6c8d104abf6d486f59279f679
jcamalpz <jcamacholpz@gmail.com>
2026-01-19 18:49:33 -0600

    Modelos

---
+++ src/api/biografia/services/biografia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * biografia service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::biografia.biografia');
